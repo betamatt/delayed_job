606c177a39925a63133a0c8ccd7f3b39fd7ab7d2
Matt Griffin <matt@griffinonline.org>
2010-07-29 15:29:53 -0400

    Fix specs when running without certain backends available

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,12 +20,12 @@ Dir.glob("#{File.dirname(__FILE__)}/setup/*.rb") do |backend|
     require "setup/#{backend}"
     require "backend/#{backend}_job_spec"
     BACKENDS << backend.to_sym
-  rescue => e 
+  rescue Exception => e
     # Allow specs to run when not all of the databases are installed.  Other exceptions should
     # still bomb the spec run so that problems are not hidden by accident.  
     # Classes are referenced by name rather than directly since they may have been the class
     # triggering a load error when the Mongo or DM gems aren't present.
-    raise unless %w(LoadError DataObjects::SQLError Mongo::ConnectionFailure).include?(e.class.name)
+    raise unless %w(MissingSourceFile LoadError DataObjects::SQLError Mongo::ConnectionFailure).include?(e.class.name)
     puts "Unable to load #{backend} backend! #{$!}"
   end
 end
